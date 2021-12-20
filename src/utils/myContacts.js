cb912ac3eac1dc1f2614d0ab1dcc06691a8a6607
Yurii-Ishchenko <Yuriyich123@gmail.com>
2021-12-20 20:46:32 +0200

    fix

--- src/utils/myContacts.js
+++ src/utils/myContacts.js
@@ -1,24 +1,23 @@
-import { AiOutlineGithub, AiFillLinkedin } from "react-icons/ai";
-import { BsFillEnvelopeFill } from "react-icons/bs";
-import { GiSmartphone } from "react-icons/gi";
+import { AiOutlineGithub, AiFillLinkedin } from 'react-icons/ai';
+import { BsFillEnvelopeFill } from 'react-icons/bs';
+import { GiSmartphone } from 'react-icons/gi';
 
 const contacts = [
-  { title: "+38(098)-26-37-74", url: "+38(098)-26-37-74", icon: GiSmartphone },
+  { title: '+38(098)-26-37-74', url: '+38(098)-26-37-74', icon: GiSmartphone },
   {
-    title: "yuriyich123@gmail.com",
-    url: "yuriyich123@gmail.com",
+    title: 'yuriyich123@gmail.com',
+    url: 'yuriyich123@gmail.com',
     icon: BsFillEnvelopeFill,
   },
   {
-    title: "https://github.com/Yurii-Ishchenko",
-    url: "https://github.com/Yurii-Ishchenko",
+    title: 'https://github.com/Yurii-Ishchenko',
+    url: 'https://github.com/Yurii-Ishchenko',
     icon: AiOutlineGithub,
   },
   {
-    title: "linkedin.com/in/yurii-ishchenko-react",
-    url: "linkedin.com/in/yurii-ishchenko-react",
+    title: 'linkedin.com/in/yurii-ishchenko-react',
+    url: 'linkedin.com/in/yurii-ishchenko-react',
     icon: AiFillLinkedin,
   },
-  // { title: "Skype", url: "www.skype.com", icon: AiFillSkype },
 ];
 export default contacts;
