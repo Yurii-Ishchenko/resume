45eec62d8f4ed89dffe7415a1fffceda65765830
Yurii-Ishchenko <Yuriyich123@gmail.com>
2022-02-02 19:37:30 +0200

    fixed

--- src/utils/myProgects.js
+++ src/utils/myProgects.js
@@ -1,23 +1,28 @@
 const progects = [
   {
-    title: "Image Finder",
-    url: "https://yurii-ishchenko.github.io/image-finder/",
-    tech: "React.js, Redux Toolkit, React-JSS",
+    title: 'Image Finder',
+    url: 'https://yurii-ishchenko.github.io/image-finder/',
+    tech: 'React.js, Redux Toolkit, React-JSS',
   },
   {
-    title: "Movies Finder",
-    url: "https://yurii-movies-found.netlify.app/",
-    tech: "React.js, Redux, React-JSS",
+    title: 'Movies Finder',
+    url: 'https://yurii-movies-found.netlify.app/',
+    tech: 'React.js, Redux, React-JSS',
   },
   {
-    title: "Phonebook",
-    url: "https://phonebook-yurii.netlify.app",
-    tech: "React.js, Redux Toolkit",
+    title: 'Phonebook',
+    url: 'https://phonebook-yurii.netlify.app',
+    tech: 'React.js, Redux Toolkit',
   },
   {
-    title: "Stopwatch",
-    url: "https://yurii-ishchenko.github.io/stopwatch/",
-    tech: "React.js, React-JSS",
+    title: 'Stopwatch',
+    url: 'https://yurii-ishchenko.github.io/stopwatch/',
+    tech: 'React.js, React-JSS',
+  },
+  {
+    title: 'Shopping cart',
+    url: 'https://yurii-shopping-cart.netlify.app/',
+    tech: 'React.js, TypeScript, Redux, React-JSS',
   },
 ];
 
--- src/utils/myTechSkills.js
+++ src/utils/myTechSkills.js
@@ -1,14 +1,16 @@
 const techSkills = [
-  "HTML5",
-  "SCC3",
-  "GIT",
-  "JavaScript",
-  "React.js",
-  "React-JSS",
-  "Redux",
-  "TypeScript",
-  "Webpack",
-  "REST API",
+  'HTML5',
+  'SCC3',
+  'SASS',
+  'GIT',
+  'JavaScript',
+  'React.js',
+  'React-JSS',
+  'Redux',
+  'TypeScript',
+  'Webpack',
+  'REST API',
+  'GraphQL',
 ];
 
 export default techSkills;
